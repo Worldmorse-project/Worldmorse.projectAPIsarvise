45c9d608755a1a386c900bb3ae53d9d999f4c0ad
worldmorse.project（official） <worldmorse.service000@gmail.com>
2026-02-16 20:00:36 +0900

    Add comment about SSL requirement for Render Postgres
    Added a comment explaining the SSL requirement for Render Postgres.

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 import pg from "pg";
+
 const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  // Render Postgres はSSLが必要になることが多い
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
